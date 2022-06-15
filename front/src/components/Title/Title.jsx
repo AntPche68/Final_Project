1f52beb56d1dd460554e34ea39893a8f952568d1
AntPche68 <ant.pchelintsev@gmail.com>
2022-06-15 21:09:46 +0300

    add title components

--- front/src/components/Title/Title.jsx
+++ front/src/components/Title/Title.jsx
@@ -1,20 +1,32 @@
 import React from 'react'
+import s from './Title.module.sass'
 import Button from '../UI/Button/Button'
 
 export default function Title() {
   return (
-    <div>
-        <div>
-            <section>
-                <h2></h2>
-                <h1></h1>
-                <p></p>
+    <div className={s.main}>
+        <div className={s.text}>
+            <section className={s.title}>
+                <h2>Welcome to Pages!!!</h2>
+                <h1>
+                    Your Books From 
+                    <br/>
+                    The Best Writer.
+                </h1>
+                <p>
+                    We believe that reading books are essential to a healthy culture.
+                    <br/>
+                    They’re where authors can connect with readers.
+                </p>
             </section>
-            <div>
+            <div className={s.btns}>
                 <Button>Order Today</Button>
                 <a href="#">Read Free Demo</a>
             </div>
-            
+
+        </div>
+        <div className={s.book}>
+            <img src="/img/book.png" alt="Book_Title" />
         </div>
     </div>
   )
