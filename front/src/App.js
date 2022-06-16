2b0464f02302e704a26f2508595dc167f58a7184
AntPche68 <ant.pchelintsev@gmail.com>
2022-06-16 19:20:02 +0300

    add TheAuthorsBook component

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './components/Main/Main';
+import TheAuthorsBook from './components/TheAuthorsBook/TheAuthorsBook';
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <TheAuthorsBook />
     </div>
   );
 }
---
+++ front/src/components/TheAuthorsBook/TheAuthorsBook.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './TheAuthorsBook.module.sass'
+
+export default function TheAuthorsBook() {
+  return (
+    <div className={s.authors_book}>
+        <h1>The Author's Book</h1>
+    </div>
+  )
+}
