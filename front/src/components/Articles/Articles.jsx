1d189998242518abbcb1d3c8611482b9a8f468e4
AntPche68 <ant.pchelintsev@gmail.com>
2022-06-24 20:54:05 +0300

    add Article styled

--- front/src/components/Articles/Articles.jsx
+++ front/src/components/Articles/Articles.jsx
@@ -17,18 +17,20 @@ export default function Articles() {
   return (
     <div className={s.main}>
         <h2>Articles & Resources</h2>
-        <section>
+        <div className={s.card}>
           {articles.map(t=>
-            <div key={t.id}>
-               <div>
+            <div className={s.article} key={t.id}>
+              <div className={s.image}>
                 <img src={process.env.PUBLIC_URL + t.image} alt="image" /> 
-                </div> 
-                <p>{t.title}</p>
-                <p>{t.abstract}</p>
-                <p>{t.date}</p>
+              </div>
+              <div className={s.text}>
+                <p className={s.title}>{t.title}</p>
+                <p className={s.abstract}>{t.abstract}</p>
+                <p className={s.date}>October 6, 2021</p>
+              </div> 
             </div>
-            )}
-        </section>
+          )}
+        </div>
     </div>
   )
 }
